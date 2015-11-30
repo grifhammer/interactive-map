46dc844c1ef3f2d3454850e75354051a10983ff2
Griffin Hammer <grifhammer@gmail.com>
2015-11-30 12:07:15 -0500

    Added ability to change a states fill color to red on click.

--- controller.js
+++ controller.js
@@ -2,11 +2,26 @@ var stateMap = angular.module('interactiveMap', []);
 
 stateMap.controller('interactiveMapCtrl', interactiveMapCtrl);
 
+stateMap.directive('clickState', function(){
+	return{
+		link: function($scope, element){
+			element.bind('click',function(){
+				var newColor = "red";
+				var stateElement = element[0].querySelector('path');
+				console.log(stateElement);
+				stateElement.setAttribute('class', 'state ' + newColor);
+			});
+		}
+	}
+});
+
+
 function interactiveMapCtrl($scope){
 	$scope.states = states;
 
 }
 
 function getNewColor(){
-	
+
 }
+
