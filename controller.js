bb75272388d965ecb7f9c6874734d023eb582c9c
Griffin Hammer <grifhammer@gmail.com>
2015-12-03 10:09:25 -0500

    Added function that sums the electoral vote totals. Also added the state info to the page that now floats over the states.

--- controller.js
+++ controller.js
@@ -8,8 +8,8 @@ stateMap.directive('clickState', function(){
 			element.bind('click',function(){
 				var newColor = getNewColor($scope.state);
 				var stateElement = element[0].querySelector('path');
-				console.log(element[0])
 				stateElement.setAttribute('class', 'state ' + newColor);
+				$scope.$parent.calculateStateTotals();
 			});
 		}
 	}
@@ -17,19 +17,51 @@ stateMap.directive('clickState', function(){
 
 
 function interactiveMapCtrl($scope){
+
+	$scope.calculateStateTotals = function(){
+		$scope.redStateVotes = 0;
+		$scope.blueStateVotes = 0;
+		$scope.openStateVotes = 0;
+		for(var i = 0; i < numStates; i++){
+			if(blueStates[i]){
+				$scope.redStateVotes += blueStates[i].electoralVotes;
+			} else if(redStates[i]){
+				$scope.blueStateVotes += redStates[i].electoralVotes;
+			} else if(openStates[i]){
+				$scope.openStateVotes += openStates[i].electoralVotes;
+			}
+			
+		}
+	}
+
+
 	$scope.states = states;
 
+	$scope.calculateStateTotals();
+
+	
+
 }
+
+
+
+
 
 function getNewColor(state){
 	if(state.stateColor === "red"){
 		state.stateColor = "blue"
+		redStates.splice(state.id,1, '');
+		blueStates[state.id] = state;
 		return "blue";
 	} else if(state.stateColor === "blue"){
 		state.stateColor = "open"
+		blueStates.splice(state.id,1, '');
+		openStates[state.id] = state;
 		return "open";
 	} else if(state.stateColor === "open"){
 		state.stateColor = "red"
+		openStates.splice(state.id,1,'');
+		redStates[state.id] = state;
 		return "red"
 	} else{
 		alert("You fucked up!")
